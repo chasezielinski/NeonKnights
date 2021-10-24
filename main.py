7551d56a8eb823c9909644981f117a583b0e3d16
Chase Zielinski <chasez444@gmail.com>
2021-10-24 12:04:11 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import sys
+import pygame
+import settings
+from menu import Menu
+# from states.game_over import GameOver
+from splash import Splash
+# from states.battle_pause import Pause
+from game import Game
+from characterselect import CharacterSelect
+from region import Region
+from battle import Battle
+from map_bounds import MapBounds
+
+# initialize the game
+# create the screen
+screen = pygame.display.set_mode((settings.X, settings.Y))
+
+# enumerate states
+states = {
+    "MENU": Menu(),
+    #    "SETUPMENU": SetupMenu(),
+    "SPLASH": Splash(),
+    "CHARACTER_SELECT": CharacterSelect(),
+    "REGION": Region(),
+    "BATTLE": Battle(),
+    "MAP_BOUNDS": MapBounds(),
+    #    "GAMEPLAY": Gameplay(),
+    #    "GAME_OVER": GameOver(),
+    #    "PAUSE": Pause(),
+}
+
+icon = settings.image_load('sword.png')
+pygame.display.set_icon(icon)
+
+# Title and Icon
+pygame.display.set_caption("Neon Knights")
+
+game = Game(screen, states, "SPLASH")
+game.run()
+
+pygame.quit()
+sys.exit()
